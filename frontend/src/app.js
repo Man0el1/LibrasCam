f74c812935261766923782f23f0fa36fe5552f79
Manoel Geremias <manoelgtcj@gmail.com>
2026-03-06 09:15:42 -0300

    Primeiro Submit

---
+++ frontend/src/app.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+
+export default function App() {
+  return (
+    <Main />
+  );
+}
